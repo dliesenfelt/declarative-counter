bb4fd58e7256c7666ec2dce6dbd359db780a4c0a
dliesenfelt <liesenfelt.code@gmail.com>
2022-03-31 20:24:39 -0700

    first push

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react'
+
+function App() {
+  let [counter, changeCounter] = useState(1)
+  window.changeCounter = changeCounter
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <h4>{counter}</h4>
+    </div>
+  )
+}
+
+export default App;
